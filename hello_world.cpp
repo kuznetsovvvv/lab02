7ad0829611452511eaba8b4ddc3fe00147766c0f
misha <mihail_160505@mail.ru>
2024-03-21 23:02:05 +0300

    delete extra lines

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,8 @@
 #include<iostream>  
 int main(){
-    setlocale(LC_ALL,"RU");
+setlocale(LC_ALL,"RU");
 std::cout<<"введите свое имя: "<<std::endl;
-std::string name;
-std::cin>>name;
+std::string name; std::cin>>name;
 std::cout<<"hello world,"<<name<<"!"<<std::endl;
-
 return 0;
 }
