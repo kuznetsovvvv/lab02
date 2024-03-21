f0a9d65bede0da21460128ff3e9f6346fd5fa03d
misha <mihail_160505@mail.ru>
2024-03-21 22:40:46 +0300

    second commit

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include<iostream>  
 using namespace std;
 int main(){
-
-
-cout<<"hello world!"<<endl;
+    setlocale(LC_ALL,"RU");
+cout<<"введите свое имя: "<<endl;
+std::string name;
+cin>>name;
+cout<<"hello world,"<<name<<"!"<<endl;
 
 return 0;
 }
