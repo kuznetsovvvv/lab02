d2c5233ca420f10101b658ed021de87e7a050e6f
misha <mihail_160505@mail.ru>
2024-03-22 08:53:48 +0300

    Changed code style using clang-format with Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
-#include<iostream>  //можно еще подкл библиотеку string
-int main(){
-setlocale(LC_ALL,"RU");
-std::cout<<"введите свое имя: "<<std::endl;
-std::string name; std::cin>>name;
-std::cout<<"hello world,"<<name<<"!"<<std::endl;//можно сразу написать имя в ""
-return 0;//стиль хорошего кода
+#include <iostream>
+int
+main()
+{
+  setlocale(LC_ALL, "RU");
+  std::cout << "введите свое имя: " << std::endl;
+  std::string name;
+  std::cin >> name;
+  std::cout << "hello world," << name << "!" << std::endl;
+  return 0;
 }
