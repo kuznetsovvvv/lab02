5ebbbff5cc739a8f554040435e8c74c37c3ad03f
misha <mihail_160505@mail.ru>
2024-03-22 09:09:20 +0300

    Updated comments: fixed punctuation and translated to another language

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
-#include<iostream>  
+#include<iostream>  //можно еще подкл библиотеку string
 int main(){
 setlocale(LC_ALL,"RU");
 std::cout<<"введите свое имя: "<<std::endl;
 std::string name; std::cin>>name;
-std::cout<<"hello world,"<<name<<"!"<<std::endl;
-return 0;
+std::cout<<"hello world,"<<name<<"!"<<std::endl;//можно сразу написать имя в ""
+return 0;//стиль хорошего кода
 }
